5a4f2313745fd2b0bd2e57c75e24888273af5378
‘Mohamad <mwahyudin@pm.me>
2023-11-15 12:33:29 +0700

    refactor service-worker

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,8 @@
+<!-- App.vue -->
 <template>
   <ion-app>
     <ion-router-outlet />
+    <!-- <ion-button @click="requestNotificationPermission()">Request Notification Permission</ion-button> -->
   </ion-app>
 </template>
 
--- src/views/Tab1Page.vue
+++ src/views/Tab1Page.vue
@@ -8,16 +8,29 @@
     <ion-content :fullscreen="true">
       <ion-header collapse="condense">
         <ion-toolbar>
-          <ion-title size="large">Tab 1</ion-title>
+          <ion-title size="large">Tab 1 notif</ion-title>
         </ion-toolbar>
       </ion-header>
 
       <ExploreContainer name="Tab 1 page" />
+      <ion-button @click="requestNotificationPermission()">Request Notification Permission</ion-button>
     </ion-content>
   </ion-page>
 </template>
 
 <script setup lang="ts">
-import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/vue';
+import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent, IonButton } from '@ionic/vue';
 import ExploreContainer from '@/components/ExploreContainer.vue';
+
+const requestNotificationPermission = () => {
+    if (Notification.permission !== 'granted') {
+      Notification.requestPermission().then(permission => {
+        if (permission === 'granted') {
+          console.log('Notification permission granted');
+        } else {
+          console.warn('Notification permission denied');
+        }
+      });
+    }
+  }
 </script>
